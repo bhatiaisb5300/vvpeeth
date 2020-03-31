fa373591f6fa20bc21f6f5238bc3ac6320d82f22
bhatiaisb5300 <52737993+bhatiaisb5300@users.noreply.github.com>
2020-03-31 12:25:57 +0530

    database logic

--- vvpeeth/settings.py
+++ vvpeeth/settings.py
@@ -74,15 +74,24 @@ WSGI_APPLICATION = 'vvpeeth.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
+if os.path.exists(os.path.join(BASE_DIR, 'db.sqlite3')):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vvp',
+        'USER': 'vvp',
+        'PASSWORD': 'vvpbhilai',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
