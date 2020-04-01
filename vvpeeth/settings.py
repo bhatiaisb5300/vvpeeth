c5b88bacc0a3c96a7436c6d7a508f2af1a03054a
bhatiaisb5300 <52737993+bhatiaisb5300@users.noreply.github.com>
2020-04-01 07:30:49 +0530

    pre final

--- vvpeeth/settings.py
+++ vvpeeth/settings.py
@@ -130,9 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static/'),
- ]
+if os.path.exists(os.path.join(BASE_DIR, 'db.sqlite3')):
+    STATICFILES_DIRS = [
+         os.path.join(BASE_DIR, 'static/'),
+     ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
